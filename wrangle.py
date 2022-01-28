c5d88ce39ce6f73524fde62ea5fcd9a0837e0b16
Joshua Wheeler <joshua.r.wheeler.27@gmail.com>
2022-01-28 08:08:34 -0600

    updated wrangle and csv, renamed exploration notebook

--- wrangle.py
+++ wrangle.py
@@ -7,7 +7,8 @@ import env
 ####### Acquire #######
 def acquire_data():
     '''
-    
+    This function acquires curriculum access logs from the codeup database
+    and returns a dataframe
     '''
 
     # saving url string to access sequel ace curriculum logs db
@@ -15,7 +16,7 @@ def acquire_data():
 
     # saving query to access corhorts and logs tables
     query_cohorts = '''
-    SELECT  id as cohortid_cohorts,
+    SELECT  id as cohortid,
             start_date,
             end_date,
             name as cohortname,
@@ -35,11 +36,29 @@ def acquire_data():
     df_logs = pd.read_sql(query_logs, url)
 
     # joining the dfs with merge on cohort id, dropping duplicate float column
-    df = df_cohorts.merge(df_logs, left_on = 'cohortid_cohorts', right_on = 'cohort_id')\
+    df = df_cohorts.merge(df_logs, left_on = 'cohortid', right_on = 'cohort_id')\
                                             .drop(columns = 'cohort_id')
 
     return df
 
+######## Prepare #######
+def prepare_logs(df):
+    '''
+    This function takes in a dataframe and creates features to assist in exploration,
+    drops null value, creates datetime features.
+    '''
+    conditions = [df.program_id == 1, df.program_id == 2, df.program_id == 3, df.program_id == 4]
+    result = ['web_dev','web_dev','data_science','web_dev']
+    df['program_name'] = np.select(conditions, result)
+    df['is_staff'] = df.isin(df[df.cohortname == 'Staff']).cohortname
+    df.dropna
+    df['date'] = pd.to_datetime(df.date)
+    df['end_date'] = pd.to_datetime(df.end_date)
+    df['start_date'] = pd.to_datetime(df.start_date)
+    # df['time'] = pd.to_datetime(df.time, format='%H:%M:%S')
+    return df
+
+####### Wrangle #######
 def get_curriculum_logs_data():
     '''
     This function reads in curriculum logs data from the Codeup database, 
@@ -48,8 +67,7 @@ def get_curriculum_logs_data():
     if os.path.isfile('curriculum_logs.csv'):
         
         # If csv file exists read in data from csv file.
-        df = pd.read_csv('curriculum_logs.csv', index_col=0)
-        
+        df = pd.read_csv('curriculum_logs.csv', index_col=0)      
         # print statements for size of each df
         print(f'The df has {df.shape[0]} rows and {df.shape[1]} columns.')
 
@@ -57,13 +75,20 @@ def get_curriculum_logs_data():
         
         # Read fresh data from db into a DataFrame
         df = acquire_data()
-        
+
+        # prepare data
+        df = prepare_logs(df)
+
         # Cache data
         df.to_csv('curriculum_logs.csv')
+
 
         # print statements for size of each df
         print(f'The df has {df.shape[0]} rows and {df.shape[1]} columns.')
 
-        
+    
     return df
+
+
+
 
